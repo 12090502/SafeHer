a71aba87df67c448dea01482f549f5e7e3773a63
Rajeswari Sadam <rajeswarisadam1209@gmail.com>
2025-11-11 12:00:00 +0530

    Implemented runtime permission requests for location and notification access to ensure secure and compliant user authorization.

--- app/src/main/java/uk/ac/tees/mad/safeher/presentation/Screens/HomeScreen.kt
+++ app/src/main/java/uk/ac/tees/mad/safeher/presentation/Screens/HomeScreen.kt
@@ -1,16 +1,48 @@
 package uk.ac.tees.mad.safeher.presentation.Screens
 
+import android.Manifest
+import android.app.Activity
+import android.content.pm.PackageManager
+import android.os.Build
+import androidx.activity.ComponentActivity
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.core.app.ActivityCompat
+
+import androidx.core.content.ContextCompat
+
 import androidx.navigation.NavController
 import uk.ac.tees.mad.safeher.presentation.ViewModel.AuthViewModel
 import uk.ac.tees.mad.safeher.presentation.ViewModel.HomeViewModel
 
 @Composable
-fun HomeScreen(modifier: Modifier = Modifier,
-               homeViewModel: HomeViewModel,
-               authViewModel: AuthViewModel,
-               navController: NavController
-               ) {
+fun HomeScreen(
+    modifier: Modifier = Modifier,
+    homeViewModel: HomeViewModel,
+    authViewModel: AuthViewModel,
+    navController: NavController,
+) {
+    val context = LocalContext.current
+    val activity = context as ComponentActivity
+
+    val permissions = mutableListOf(
+        Manifest.permission.ACCESS_FINE_LOCATION,
+        Manifest.permission.ACCESS_COARSE_LOCATION
+    )
+
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+        permissions.add(Manifest.permission.POST_NOTIFICATIONS)
+    }
+    val notGranted = permissions.filter {
+        ContextCompat.checkSelfPermission(context, it) != PackageManager.PERMISSION_GRANTED
+    }
+    if (notGranted.isNotEmpty()) {
+        ActivityCompat.requestPermissions(
+            activity,
+            notGranted.toTypedArray(),
+            101
+        )
+    }
 
 }
--- app/src/main/java/uk/ac/tees/mad/safeher/presentation/ViewModel/HomeViewModel.kt
+++ app/src/main/java/uk/ac/tees/mad/safeher/presentation/ViewModel/HomeViewModel.kt
@@ -1,13 +1,17 @@
 package uk.ac.tees.mad.safeher.presentation.ViewModel
 
+import android.Manifest
+import android.content.pm.PackageManager
+import androidx.activity.ComponentActivity
+import androidx.compose.ui.platform.LocalContext
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
 class HomeViewModel @Inject constructor() : ViewModel() {
-
-
 
 
 
